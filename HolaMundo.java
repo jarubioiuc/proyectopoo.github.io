25424cee6f9d6b9c9f434a5bd198ba9152bc4e1f
jarubioiuc <jarubio.iuc@outlook.com>
2023-06-16 09:17:27 -0500

    primer commit

---
+++ HolaMundo.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class HolaMundo here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+public class HolaMundo
+{
+    public static void main(String[] args){
+        System.out.println("Hola Mundo");
+        
+        
+        String varTexto = "Esto es una variable texto";
+        double varDouble = 150000000;
+        
+        Scanner obj_sc = new Scanner(System.in);
+        
+        System.out.println(varTexto);
+        System.out.println(varDouble);
+        
+        
+    }
+}
